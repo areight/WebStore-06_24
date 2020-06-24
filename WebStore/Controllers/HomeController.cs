485aae0a9abcba27c0e4d89cf3bfc93ae7fa297c
ar8artemiy <47075726+ar8artemiy@users.noreply.github.com>
2020-06-24 20:21:45 +0300

    First controller!

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Controller #1 - Home!");
+            // return View();
+        }
+    }
+}
