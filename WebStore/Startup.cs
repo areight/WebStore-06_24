73a1385f8fef973ffbeafdd007ff7fe55392224a
ar8artemiy <47075726+ar8artemiy@users.noreply.github.com>
2020-06-24 20:17:18 +0300

    Added MVC services into the app logic

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,29 +13,46 @@ namespace WebStore
 {
     public class Startup
     {
+        private readonly IConfiguration _Configuration;
+        public Startup (IConfiguration Configuration)
+        {
+            _Configuration = Configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            {                                    // Handles exceptions (for developers),
+                app.UseDeveloperExceptionPage(); // increases safety & hides file structure
+            }                                    // by not showing the error message                      (?)
 
-            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
+
+            app.UseRouting(); // Routing
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGet("/greetings", async context => await context.Response.WriteAsync(_Configuration["CustomGreetings"]));
+
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Response");
                 });
+
+                endpoints.MapControllerRoute(
+                name:"default",
+                pattern:"{controller=Home}/{action=Index}/{id?}");
             });
+            // Handles endpoints.
+            // An endpoint is the parts after a slash (except the first one?)
         }
     }
 }
